21a3713c029c8bde3b1f9f01127a6720d7646f82
hunterxcobby <solomonsefah13@gmail.com>
2023-06-22 07:40:45 +0000

    Added files

---
+++ grades.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int score = 80;
+
+	if(score >= 70)
+       	{
+		printf("Excellent!\n");
+		printf("You have passed the mark %d\n", score);
+       
+	} else {
+		
+		printf("sorry but you have failed!\n");
+	}
+
+        return (0);
+}

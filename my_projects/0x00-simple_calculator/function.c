8e96905276d0a2fedf04f8316bf0b0f743acc481
hunterxcobby <solomonsefah13@gmail.com>
2023-09-28 23:30:59 +0000

    removed the while loop

--- my_projects/0x00-simple_calculator/function.c
+++ my_projects/0x00-simple_calculator/function.c
@@ -4,25 +4,25 @@
 int add(int count, ...)
 {
 
-    va_list numbers;
-    int result;
-    int i;
-    va_start(numbers, count);
+	va_list numbers;
+	int result;
+	int i;
+	va_start(numbers, count);
 
-    result = 0;
-    
-    /* accessing all members of the list*/
-    for (i = 0; i < count; i++){
-        result = result + va_arg(numbers, int);
-    }
+	result = 0;
 
-    /*while((value = va_arg(numbers, int)) != 0)
-    {
-	    result += value;
-    }*/
-    
-    va_end(numbers);
-    return (result);
+	/* accessing all members of the list*/
+	for (i = 0; i < count; i++){
+		result = result + va_arg(numbers, int);
+	}
+
+	/*while((value = va_arg(numbers, int)) != 0)
+	  {
+	  result += value;
+	  }*/
+
+	va_end(numbers);
+	return (result);
 }
 
 
@@ -52,7 +52,7 @@ int multiply(int count, ...)
 {
 	va_list numbers;
 	int result = 1; /*Initialize with 1 for multiplication*/
-        int i;
+	int i;
 	va_start(numbers, count);
 
 	/* accessing all members of the list */
@@ -67,30 +67,30 @@ int multiply(int count, ...)
 /* DIVISION */
 double divide(int count, ...) 
 {
-    va_list numbers;
-    double result;
-    int i;
-    double next_value;
-    va_start(numbers, count);
+	va_list numbers;
+	double result;
+	int i;
+	double next_value;
+	va_start(numbers, count);
 
-    result = va_arg(numbers, double); /*Initialize with the first value*/
+	result = va_arg(numbers, double); /*Initialize with the first value*/
 
-    /* accessing all members of the list (starting from the second value) */
-    for (i = 1; i < count; i++) 
-    {
-        next_value = va_arg(numbers, double);
-        
-        if (next_value != 0) {
-            result /= next_value;
-        } else {
-            printf("Error: Division by zero\n");
-            va_end(numbers);
-            return 0.0;
-        }
-    }
+	/* accessing all members of the list (starting from the second value) */
+	for (i = 1; i < count; i++) 
+	{
+		next_value = va_arg(numbers, double);
 
-    va_end(numbers);
-    return result;
+		if (next_value != 0) {
+			result /= next_value;
+		} else {
+			printf("Error: Division by zero\n");
+			va_end(numbers);
+			return 0.0;
+		}
+	}
+
+	va_end(numbers);
+	return result;
 }
 
 /* MIXED OPERATIONS */
@@ -107,7 +107,8 @@ double mixed(int count, ...)
 	result = va_arg(args, double); 
 
 
-	for (i = 1; i < count; i += 2) {
+	for (i = 1; i < count; i += 2)
+	{
 		operation = va_arg(args, int);
 		value = va_arg(args, double);
 
@@ -137,6 +138,7 @@ double mixed(int count, ...)
 				va_end(args);
 				return 0;
 		}
+
 	}
 
 	va_end(args);

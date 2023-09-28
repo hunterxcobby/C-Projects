012f8c1721b7982ca874b3286cdde62404c374db
hunterxcobby <solomonsefah13@gmail.com>
2023-09-28 23:03:19 +0000

    restructed the whole code in mixed operations

--- my_projects/0x00-simple_calculator/mixed_operations.c
+++ my_projects/0x00-simple_calculator/mixed_operations.c
@@ -2,46 +2,51 @@
 
 void mixed_operations(void)
 {
+    int max_operations = 15;
+    int operation_count = 0;
+    char operation;
+    double number;
+    int i;
+    double result;
+    double numbers[15];
+    char operations[15];
 
-	int max_operations = 15;
-	int operation_count = 0;
-        char operation;
-        double number;
-	int i;
+    /* Collect operations and numbers from the user*/
+    printf("Enter operations and numbers (enter 0 as operation to finish):\n");
 
-        double result;
+    while (1) 
+    {
+        scanf(" %c", &operation); /* Note the space before %c to skip whitespace characters */
+        
+        if (operation == '0') 
+        {
+            break; /* Exit the loop when '0' is entered as operation*/
+        }
 
-	double numbers[15];
-	char operations[15];
+        /* Make sure to also get the corresponding number */
+        scanf("%lf", &number); 
 
+        if (operation_count < max_operations) /* Check if max_operations limit is not reached*/
+        {
+            operations[operation_count] = operation;
+            numbers[operation_count] = number;
+            operation_count++;
+        }
+        else
+        {
+            printf("Maximum number of operations reached.\n");
+            break; /* Exit the loop if max_operations limit is reached*/
+        }
+    }
 
-	/* Collect operations and numbers from the user*/
-	printf("Enter operations and numbers (enter 0 as operation to finish):\n");
-	while (1) 
-	{
-		scanf(" %c", &operation); /* Note the space before %c to skip whitespace characters*/
-		if (operation == '0') 
-		{
-			break;
-		}
+    /* Call the calculate function with collected data */
+    result = numbers[0];
 
-		if (operation != '0') 
-		{
-			operations[operation_count] = operation;
-			numbers[operation_count] = number;
-			operation_count++;
+    for (i = 0; i < operation_count; i++) 
+    {
+        result = mixed(2, result, operations[i], numbers[i + 1]);
+    }
 
-		if (operation_count == max_operations) {
-			printf("Maximum number of operations reached.\n");
-			break;
-		}
-	}
-
-	/*Call the calculate function with collected data*/
-	result = numbers[0];
-
-	for (i = 0; i < operation_count; i++) {
-    result = mixed(2, result, operations[i], numbers[i + 1]);
+    printf("Result: %.2f\n", result);
 }
-	printf("Result: %.2f\n", result);
-}
+

37c9c299088fc057e46b5d138d37eb5c6ffdb83f
Cobby Sefah Solomon <solomonsefah13@gmail.com>
2023-10-06 17:16:26 +0000

    Create 14-getline.c

---
+++ lessons/simple_shell/allowed_functions/14-getline.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) 
+{
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    printf("Enter a line of text: ");
+
+    /* Read a line of text from stdin */
+    read = getline(&line, &len, stdin);
+
+    if (read != -1) {
+        printf("Line read (%zd bytes): %s", read, line);
+    } else {
+        printf("Error reading line\n");
+    }
+
+    /* Free the allocated memory */
+    free(line);
+
+    return 0;
+}

35c5fdc947e242f139a6ea668409801fe69ce2fe
hunterxcobby <solomonsefah13@gmail.com>
2023-10-09 13:10:18 +0000

    executed program

--- simple_shell/shell.c
+++ simple_shell/shell.c
@@ -26,8 +26,40 @@ int main(void)
         token = strtok(line, delimiters);
         while(token != NULL)
         {
-            printf("%s\n", token);
-            token = strtok(NULL, delimiters);
+            /**
+             * Here we can check if the token is a built-in command like (i.e, cd, exit)
+             * And handle it separately  
+             */
+
+            /* If it is not a built in command, we execute it*/
+            pid_t pid = fork();
+
+            if (pid == 1)
+            {
+                perror("Fork failed");
+                return(-1);
+            }
+            else if (pid == 0)
+            {
+                /* This is the child process*/
+                char *cmdPath = get_path(token);/* Assuming we have a function called getpath to find the command's path*/
+                if (cmdPath == NULL)
+                {
+                    perror("Command not found");
+                    exit(1);
+                }
+
+                /* We execute the command with execve*/
+                execve(cmdPath, &token, __environ);
+
+                /* Check if evecve fails*/
+                if (execve(cmdPath, &token, __environ) == -1)
+                {
+                    perror("Failed to execute program");
+                    exit(EXIT_FAILURE); /* Indicate the command could not be executed*/
+                }
+
+            }
         }
 
         return(0);

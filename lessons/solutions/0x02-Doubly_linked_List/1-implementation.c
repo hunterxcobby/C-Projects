44bb6acf56784b45a10acd2633d34da44152fa72
hunterxcobby <solomonsefah13@gmail.com>
2023-10-12 14:29:43 +0000

    introducing a temporary variable

--- lessons/solutions/0x02-Doubly_linked_List/1-implementation.c
+++ lessons/solutions/0x02-Doubly_linked_List/1-implementation.c
@@ -11,10 +11,16 @@ typedef struct node
 /* This is the head pointer that points to the first node*/
 node *head; 
 node *newnode;
+/* Set head to 0*/
+node *head = NULL;
+/* We need a temp to store tempory*/
+node *temp = NULL; /* This is be holding nodes when we need a third node*/
 
 /* A function to create new node*/
 void create_node(void)
 {
+    
+
     /* ALlocate memory for the new node*/
      newnode = malloc(sizeof(struct node ));/* 20 bytes will be allocated*/
 
@@ -27,6 +33,21 @@ void create_node(void)
      newnode->next = 0;
      newnode->prev = 0; /* Because they do not point to anything yet*/
      printf("Address: %p\n", newnode->next);/*uncomment to see address of next node*/
+
+    /* Insert data as the first node*/
+    if (head == 0)
+    {
+        /* If head is empty then we insert this as first node*/
+        head = temp = newnode;
+    }
+    else /* Insert data as the next node*/
+    {
+        /* Point the first node's address to the next node*/
+        temp->next = newnode; /* Link the newnode to the previous node*/
+        /* Point the new node's address to the previous node*/
+        newnode->prev = head; /* Link the previous node to the new node */
+
+    }
 
      free(newnode); /* Remember to free node after using*/
 }

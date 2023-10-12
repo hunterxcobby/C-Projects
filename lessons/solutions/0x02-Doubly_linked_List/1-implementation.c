54a6bf8b29f96551eebde18fa29638123826709b
hunterxcobby <solomonsefah13@gmail.com>
2023-10-12 14:00:21 +0000

    remember to free the new node

--- lessons/solutions/0x02-Doubly_linked_List/1-implementation.c
+++ lessons/solutions/0x02-Doubly_linked_List/1-implementation.c
@@ -17,13 +17,18 @@ void create_node(void)
 {
     /* ALlocate memory for the new node*/
      newnode = malloc(sizeof(struct node ));/* 20 bytes will be allocated*/
+
+     /* Take data from user*/
      printf("Enter the data:\n");
      scanf("%d", &newnode->data);/* You can also write "(*newnode).data"*/
 
+    /* Display the data to them*/
      printf("Data:%d\n", newnode->data);
-     /*printf("Address: %p\n", newnode->next);uncomment to see address of next node*/
+     newnode->next = 0;
+     newnode->prev = 0; /* Because they do not point to anything yet*/
+     printf("Address: %p\n", newnode->next);/*uncomment to see address of next node*/
 
-     
+     free(newnode); /* Remember to free node after using*/
 }
 
 int main(void)

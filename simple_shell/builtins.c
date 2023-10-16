f362553329f150b19a8fcb00656005bbcd52092e
hunterxcobby <solomonsefah13@gmail.com>
2023-10-16 12:44:18 +0000

    I overwrote this with the codes from the main Shell Repo project

--- simple_shell/builtins.c
+++ simple_shell/builtins.c
@@ -1,44 +1,42 @@
 #include "main.h"
 
-/* In this file, we check if command is a builtin*/
+/* This file used for built-in commands */
 
-void builtin_cd(char *arg)/* Remember our function returns void*/
-{   
-    /* If the user does not pass any argument to it, we take them home*/
-    if (arg == NULL) 
+/* When the user enters cd*/
+void builtin_cd(char *args)
+{
+  /* If the user does not pass any argument*/
+  if (args == NULL)
+  {
+    char *home = getenv("HOME");
+    
+    if(home == NULL)
     {
-        char *home = getenv("HOME");
-        /* If the person does not have the HOME env set*/
-        if (home == NULL) 
-        {
-            fprintf(stderr, "cd: HOME environment variable not set\n");
-        }
-        else
-        {
-            /* This will take the user home*/
-            if (chdir(home) != 0) 
-            {
-                perror("Could not change directory");
-            }
-        } 
+        perror("cd: HOME envrionment variable is not set\n");
     }
-    else 
+    else
     {
-        /* We change directory*/
-        if (chdir(arg) != 0) 
+        /* Take the user home*/
+        if (chdir(home) != 0)
         {
-            perror("Could not change directory");
+            perror("could not change directory");
         }
     }
+  }
+  else
+  {
+     /* If the user adds an argument*/
+     if (chdir(args) != 0)
+        {
+            perror("could not change directory");
+        }
+  }
 }
 
-
-/* THis is for exiting*/
-void builtin_exit() 
+void builtin_exit(void)
 {
-    /* Remember clear also works*/
-    printf("Exiting the shell...\n");
+    printf("\nExiting shell...\n");
     /* Before it exists, it can sleep*/
     sleep(2);
-    exit(0);/* To indicate success*/
+    exit(0); /* Successfully exit*/
 }

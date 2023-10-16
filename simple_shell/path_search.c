bb0feee07b9699f3c3e036003504d07125acddb1
hunterxcobby <solomonsefah13@gmail.com>
2023-10-16 12:50:00 +0000

    I overwrote this with the codes from the main Shell Repo project

--- simple_shell/path_search.c
+++ simple_shell/path_search.c
@@ -1,41 +1,43 @@
 #include "main.h"
 
 /**
- * This basically contains
- * the path to search
+ * This contains codes
+ * for searching through
+ * the path 
  */
 
 char *get_path(char *command) /* It takes the tokenized string as argument and calls it a command*/
 {
-
     char *path = getenv("PATH");
     char *path_copy = strdup(path); /* Not to modify the original environment variable*/
-
+    /* Copy of the command*/
+    char command_path[1024];
     char *dir = strtok(path_copy, ":");
 
     /* A loop to iterate and concatenate*/
     while (dir != NULL)
     {
-        char *full_path = malloc(strlen(dir) + strlen(command) + 2);
-        sprintf(full_path, "%s/%s", dir, command);
-        /*printf("Constructed full_path: %s\n", full_path);*/
-
-        /* To see what is in full_path*/
-        printf("Checking path: %s\n", full_path);
-
-        /* Check if the file exists */
-        if (access(full_path, F_OK) == 0 || access(full_path, X_OK) == 0)
+        /* Create a copy of the directory*/
+        strcpy(command_path, dir);
+        if (command_path[strlen(command_path)- 1] != '/')
         {
-            printf("File found\n");
-            free(path_copy); /* Free path_copy since it's not used anymore */
-            return full_path; /* Return the full path */
+            strcat(command_path, "/");
         }
 
-        free(full_path);
+        /* Concatenate the path with the command*/
+        strcat(command_path,command);
+        
+        /* Check if the file exists and is executable */
+        if (access(command_path, F_OK) == 0 && access(command_path, X_OK) == 0)
+        {
+            free(path_copy); /* Free path_copy since it's not used anymore */
+            return (strdup(command_path)); /* Return the full path */
+        }
+
+        /*free(path_copy);*/
         dir = strtok(NULL, ":"); /* Proceed to the next directory*/
     }
 
-    
     free(path_copy);
     /* if no executable is found, it returns NULL*/
     return (command);

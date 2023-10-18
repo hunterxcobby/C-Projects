36932836da7dde0d39a18b119acdb05fb4c6fb02
hunterxcobby <solomonsefah13@gmail.com>
2023-10-18 23:48:13 +0000

    added the main program

--- monty/monty.c
+++ monty/monty.c
@@ -6,8 +6,9 @@
 
 int main(int argc, char *argv[])
 {
-    int fd;
-    /*char *file_name;*/
+    int fd;/* The file descriptor*/
+    char line[1024];/* Initialize a buffer to read lines from the file */
+    int line_number = 1;/* LIne number will always start at one*/
 
     /* user does not give any file or more than one argument to your program*/
     if (argc != 2 || !argv[1])
@@ -22,9 +23,9 @@ int main(int argc, char *argv[])
     if (fd == -1)
     {
         fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		close(fd);
 		exit(EXIT_FAILURE);
     }
 
+    close(fd);
     return (0);
 }

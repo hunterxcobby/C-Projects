64b24c9502437814878896dc484c3b8d00836082
hunterxcobby <solomonsefah13@gmail.com>
2023-10-29 10:37:43 +0000

    added header file

--- monty/main.h
+++ monty/main.h
@@ -1,10 +1,56 @@
 #ifndef MAIN_H
 #define MAIN_H
+#define SIZE 50 /* Define the size of the stack*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
+
+
+/* Global declaration of variables for testing */
+
+extern int stack[SIZE]; /* Array implementation of stack*/
+extern int top; /* To insert and delete the data elements in the stack*/
+extern int i; /* To traverse the loop to while displaying the stack*/
+extern int choice; /* To choose either of the 3 stack operations*/
+
+/* DATA STRUCTURES*/
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+        int n;
+        struct stack_s *prev;
+        struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+        char *opcode;
+        void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/* FUNCTION PROTOTYPES GOES HERE*/
+
+
 
 #endif

e7ee3ff9fa7bd34768ab1554e521e1a36a286368
hunterxcobby <solomonsefah13@gmail.com>
2023-10-29 11:51:27 +0000

    build an online registration forms

---
+++ 01-registration_forms/info.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/* Display Model Infos*/
+void mode_msg(void)
+{
+    centerAlignText("--- ONLINE REGISTRATION FORM TEMPLATE ---\n");
+    printf("This is for learning purpose and no info will be saved.\n");
+    printf("--------------------------------------------------------\n");
+    printf("RELEASE DATE: \033[2m01-08-2023\033[0m\n");
+    printf("RELEASE TIME: \033[2m20:09:30 GMT\033[0m\n");
+    printf("AUTHOR: Cobby Sefah \033[2m<@hunterxcobby>\033[0m\n");
+    printf("--------------------------------------------------------\n\n\n\n");
+}
+
+/* Display Message*/
+void title_msg(void)
+{
+    centerAlignText("ABC REGISTRATION FORMS");
+    centerAlignText("======================");
+
+
+    printf("\nPlease fill out the form for registration.\n");
+}
+
+/* The User's Name*/
+/* The funtion for user's name was removed from here*/

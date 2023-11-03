fe8cdaa8c663e67e7ff4267bf059490a7a5befcc
hunterxcobby <solomonsefah13@gmail.com>
2023-11-03 17:45:12 +0000

    tried the first sample

---
+++ lessons/solutions/0x05-Hash-tables/introduction/0-sample.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define CAPACITY 50000 // Size of the HashTable.
+
+unsigned long hash_function(char* str)
+{
+    unsigned long i = 0;
+
+    for (int j = 0; str[j]; j++)
+    {
+        i = i + str[j];
+        printf("%ld => ", i);
+        printf("Ascii value for %c is %d\n ", str[j], str[j]);
+    }
+    return i % CAPACITY;
+}
+
+int main(void)
+{
+    hash_function("Cobby");
+
+    return(0);
+}

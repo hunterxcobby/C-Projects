39eed9f98f31da7ca45c8bf73990e7aadf5c08a2
hunterxcobby <solomonsefah13@gmail.com>
2023-11-03 21:30:01 +0000

    the hash function we will be using

---
+++ lessons/solutions/0x05-Hash-tables/introduction/hash_function.c
@@ -0,0 +1,17 @@
+#include "def_struct.h"
+
+#define CAPACITY 50000 /* Size of the HashTable.*/
+
+unsigned long hash_function(char* str)
+{
+    int j;
+    unsigned long i = 0;
+
+    for (j = 0; str[j]; j++)
+    {
+        i = i + str[j];
+        printf("%ld => ", i);
+        printf("Ascii value for %c is %d\n ", str[j], str[j]);
+    }
+    return i % CAPACITY;
+}

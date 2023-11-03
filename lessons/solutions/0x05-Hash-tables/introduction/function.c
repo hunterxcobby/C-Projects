c84f969d13df8ee2f01a3e20644615bac9ba8695
hunterxcobby <solomonsefah13@gmail.com>
2023-11-03 18:33:05 +0000

    a function that contains the different functions of creating a new hash table

---
+++ lessons/solutions/0x05-Hash-tables/introduction/function.c
@@ -0,0 +1,32 @@
+#include "def_struct.h"
+
+ht_item* create_item(char* key, char* value)
+{
+    /*Creates a pointer to a new HashTable item.*/
+
+    /* ALlocate memory for the items*/
+    ht_item* item = (ht_item*) malloc(sizeof(ht_item));
+    item->key = malloc(strlen(key) + 1);
+    item->value = malloc(strlen(value) + 1);
+    
+    /* return a copy the item */
+    strcpy(item->key, key);
+    strcpy(item->value, value);
+    return item;
+}
+
+hashtable* create_table(int size)
+{
+    /* Create a new hash table*/
+    hashtable *table = malloc(sizeof(hashtable));
+    table->size = size;
+    table->count = 0;
+    table->items = calloc(table->size, sizeof(ht_item*));
+
+    /* TO set all the elements of the hashtable to null*/
+    for (int i = 0; i < table->size; i++)
+        table->items[i] = NULL;
+
+    return table;
+
+}

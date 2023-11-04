d3a11b11549b08fab3142b1985879945a2ddd54d
hunterxcobby <solomonsefah13@gmail.com>
2023-11-04 13:56:27 +0000

    add comments with real life example to explain the codes

--- lessons/solutions/0x05-Hash-tables/introduction/display_table.c
+++ lessons/solutions/0x05-Hash-tables/introduction/display_table.c
@@ -1,15 +1,19 @@
 #include "def_struct.h"
 
+/* This function prints the contents of a HashTable. */
+
 void print_table(hashtable* table)
 {
     int i;
 
     printf("\nHash Table\n-------------------\n");
 
+    /* Go through each slot in the table. */
     for (i = 0; i < table->size; i++)
     {
         if (table->items[i])
         {
+            /* If there's something stored in this slot, print its details. */
             printf("Index:%d, Key:%s, Value:%s\n", i, table->items[i] -> key, table->items[i]->value);
         }
     }

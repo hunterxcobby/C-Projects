c97fd3f9a993d901fcae49c9b5de34ceced8e63a
hunterxcobby <solomonsefah13@gmail.com>
2023-11-04 15:23:01 +0000

    include function prototypes

--- lessons/solutions/0x05-Hash-tables/introduction/def_struct.h
+++ lessons/solutions/0x05-Hash-tables/introduction/def_struct.h
@@ -28,12 +28,30 @@ typedef struct hashtable
 /* Defines the LinkedList. */
 typedef struct LinkedList 
 {
-    ht_item* item; /* Pointer to an item in the HashTable. */
-    struct LinkedList* next; /* Pointer to the next node in the linked list. */
+    ht_item *item; /* Pointer to an item in the HashTable. */
+    struct LinkedList *next; /* Pointer to the next node in the linked list. */
 } LinkedList;
 
 
-ht_item* create_item(char* key, char* value);
-hashtable* create_table(int size);
+/* The function prototypes*/
+
+ht_item *create_item(char *key, char *value);
+hashtable *create_table(int size);
+unsigned long hash_function(char *str);
+void ht_insert(hashtable *table, char *key, char *value);
+ht_item *create_item(char *key, char *value);
+LinkedList *allocate_list();
+LinkedList *linkedlist_insert(LinkedList *list, ht_item *item);
+ht_item *linkedlist_remove(LinkedList *list);
+void free_linkedlist(LinkedList *list);
+LinkedList **create_overflow_buckets(hashtable *table);
+void free_overflow_buckets(hashtable *table);
+void handle_collision(hashtable *table, unsigned long index, ht_item *item);
+char *ht_search(hashtable *table, char *key);
+void print_search(hashtable *table, char *key);
+void ht_delete(hashtable *table, char *key);
+void print_table(hashtable *table);
+void free_item(ht_item *item);
+void free_table(hashtable *table);
 
 #endif /*DEF_STRUCT_H*/

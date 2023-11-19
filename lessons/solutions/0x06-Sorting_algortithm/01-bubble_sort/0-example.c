b20bf935c06f7f8e85ce5b4c358204a2f9272489
hunterxcobby <solomonsefah13@gmail.com>
2023-11-19 21:00:28 +0000

    the bubble sort technique

---
+++ lessons/solutions/0x06-Sorting_algortithm/01-bubble_sort/0-example.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i;
+    int num[] = { 15, 16, 6, 8, 5};
+
+
+    /* SInce we are using the bubble sort technique*/
+    /*for (i = 0; i < 6; i++)
+    {
+        printf("[%d]\n", num[i]);
+    }*/
+
+
+    return (0);
+}

da7656fc8a7bbd699f5a171060131afab8694c98
hunterxcobby <solomonsefah13@gmail.com>
2023-11-20 18:21:41 +0000

    header file

---
+++ lessons/solutions/0x06-Sorting_algorithm/04-quick_sort/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int partition(int *array, int high, int low);
+int quicksort(int *array, int high, int low);
+void swap(int a, int b);
+
+#endif 

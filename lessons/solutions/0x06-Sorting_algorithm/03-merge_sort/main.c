ccedd5a24f5ff70f887f093c7d9f53a1dee85dfb
hunterxcobby <solomonsefah13@gmail.com>
2023-11-20 21:50:44 +0000

    implement main function

---
+++ lessons/solutions/0x06-Sorting_algorithm/03-merge_sort/main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+int main(void)
+{
+    int arr[] = {12, 11, 13, 5, 6, 7};
+    int arr_size = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original Array: ");
+    for (int i = 0; i < arr_size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+
+    mergeSort(arr, 0, arr_size - 1);
+
+    printf("Sorted Array: ");
+    for (int i = 0; i < arr_size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+
+    return 0;
+}

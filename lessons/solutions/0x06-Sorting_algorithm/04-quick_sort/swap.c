40cdab70d4fe3f8f41d39a3cbbb17e2127d06e84
hunterxcobby <solomonsefah13@gmail.com>
2023-11-20 18:34:59 +0000

    add comment

--- lessons/solutions/0x06-Sorting_algorithm/04-quick_sort/swap.c
+++ lessons/solutions/0x06-Sorting_algorithm/04-quick_sort/swap.c
@@ -1,8 +1,25 @@
 #include "main.h"
 
+/**
+ * Function to swap the values of two integers
+*/
+
 void swap(int *a, int *b)
 {
+    /* Create a temporary variable to store the value of 'a'*/
     int temp = *a;
+
+    /* Update the value of 'a' with the value of 'b'*/
     *a = *b;
+
+    /* Update the value of 'b' with the temporary variable (original value of 'a')*/
     *b = temp;
 }
+
+/**
+ * The function swap takes two integer pointers (a and b) as parameters.
+ * It uses a temporary variable (temp) to store the original value of a.
+ * it updates the value of a with the value of b.
+ * Finally, it updates the value of b with the original value of a stored in the temporary variable.
+*/
+

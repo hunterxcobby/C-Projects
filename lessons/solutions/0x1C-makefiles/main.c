b093b453ff6bfdaec9e6d61b59c24ba780990729
hunterxcobby <solomonsefah13@gmail.com>
2023-12-24 12:47:33 +0000

    to test make file

---
+++ lessons/solutions/0x1C-makefiles/main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+int main (void)
+{
+    printf("This is to explain the use of makefiles\n");
+    printf("This is the code in the main program\n");
+
+    function();
+    return (0);
+}
